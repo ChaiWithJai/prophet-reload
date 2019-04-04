764dc44a43ba2a0a166e1b3193cdf7d26064d99f
Jay Bhagat <user.email>
2019-04-04 09:47:43 -0400

    turned ParentContainer to dumb component

--- client/components/Template/ParentContainer.js
+++ client/components/Template/ParentContainer.js
@@ -1,30 +1,22 @@
-import React, {Component} from 'react'
-import {connect} from 'react-redux'
-import {withRouter} from 'react-router'
+import React from 'react'
 import Dashboard from './Dashboard'
 import UserPortfolio from './UserPortfolio'
 import Header from './Header'
 import Selector from './Selector'
 
-export default class ParentContainer extends Component {
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-    return (
-      <div className="parent-container">
-        <div className="header-container">
-          <Header />
-        </div>
-        <div className="portfolio-dashboard-selector-container">
-          <UserPortfolio />
-
-          <Dashboard />
-
-          <Selector />
-        </div>
+const ParentContainer = () => {
+  return (
+    <div className="parent-container">
+      <div className="header-container">
+        <Header />
       </div>
-    )
-  }
+      <div className="portfolio-dashboard-selector-container">
+        <UserPortfolio />
+        <Dashboard />
+        <Selector />
+      </div>
+    </div>
+  )
 }
+
+export default ParentContainer
