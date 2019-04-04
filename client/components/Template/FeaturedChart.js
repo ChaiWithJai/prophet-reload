e9f7389b07a8b4ac679db8d298625bcd17db9b13
Jay Bhagat <user.email>
2019-04-04 09:43:38 -0400

    turned FeaturedChart into dumb component

--- client/components/Template/FeaturedChart.js
+++ client/components/Template/FeaturedChart.js
@@ -1,18 +1,12 @@
-import React, {Component} from 'react'
+import React from 'react'
 import HomePageChart from '../HomePageChart'
-import {connect} from 'react-redux'
-import {withRouter} from 'react-router'
 
-export default class FeaturedChart extends Component {
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-    return (
-      <div className="dashboard-feature">
-        <HomePageChart />
-      </div>
-    )
-  }
+const FeaturedChart = () => {
+  return (
+    <div className="dashboard-feature">
+      <HomePageChart />
+    </div>
+  )
 }
+
+export default FeaturedChart
