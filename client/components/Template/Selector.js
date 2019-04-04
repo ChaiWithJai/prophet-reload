81013d3c3550787107ce4a5e396854671bc313e3
Jay Bhagat <user.email>
2019-04-04 14:38:50 -0400

    selector turned to dumb component

--- client/components/Template/Selector.js
+++ client/components/Template/Selector.js
@@ -1,23 +1,16 @@
-import React, {Component} from 'react'
-import {connect} from 'react-redux'
-import {withRouter} from 'react-router'
+import React from 'react'
 import Search from './Search'
 import CompanyPrices from './CompanyPrices'
 import PeerSelections from './PeerSelections'
 
-export default class Selector extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {}
-  }
-
-  render() {
-    return (
-      <div className="selector-container">
-        <Search />
-        <CompanyPrices />
-        <PeerSelections />
-      </div>
-    )
-  }
+const Selector = () => {
+  return (
+    <div className="selector-container">
+      <Search />
+      <CompanyPrices />
+      <PeerSelections />
+    </div>
+  )
 }
+
+export default Selector
