0812dfc68c90b3df566d5315bc9b86ff9f932518
Jay Bhagat <user.email>
2019-04-05 11:50:08 -0400

    AssetAllocation Modal seperate comp and added useEffect n useState hooks

--- client/components/AssetAllocation.js
+++ client/components/AssetAllocation.js
@@ -1,15 +1,10 @@
-import React, {Component} from 'react'
+import React, {Component, useState, useEffect} from 'react'
 import {getPortfolio} from '../store/assetallocation'
 import {getStockPriceForAssetAllocation} from '../store/chart'
 import {connect} from 'react-redux'
 import {withRouter} from 'react-router'
 import {RadialChart} from 'react-vis'
-import {
-  Button,
-  Modal,
-  Transition,
-  TransitionablePortal
-} from 'semantic-ui-react'
+import {Button, Modal, Transition, SemanticCOLORS} from 'semantic-ui-react'
 
 const myPalette = [
   '#cc00ff',
@@ -26,6 +21,106 @@ const myPalette = [
   'ae1865'
 ]
 
+const AssetAllocationModal = (props, initialOpen = false, size = 'tiny') => {
+  const [open, setOpen] = useState(initialOpen)
+  const show = () => setOpen(true)
+  //original incase not working const show = size => () => this.setState({size, open: true})
+  const close = () => setOpen(false)
+  return (
+    <div>
+      <h4>
+        Portfolio Allocation{' '}
+        <Button onClick={show} id="help-btn" icon="question circle outline" />{' '}
+      </h4>
+      <div>
+        <Transition.Group
+          open={open}
+          transition="horizontal-flip"
+          duration={1000}
+        >
+          {open === true && (
+            <Modal size={size} open={open} onClose={close}>
+              <Modal.Header>
+                This section contains a graphical division of your portfolio
+              </Modal.Header>
+              <Modal.Content>
+                <p>
+                  This pie chart graph will automatically calculate and display
+                  how your initial cash was divided between your bought stocks.
+                </p>
+              </Modal.Content>
+              <Modal.Actions>
+                <Button
+                  onClick={close}
+                  positive
+                  circular={true}
+                  icon="checkmark"
+                  labelPosition="left"
+                  content="Gang Gang"
+                />
+              </Modal.Actions>
+            </Modal>
+          )}
+        </Transition.Group>
+      </div>
+    </div>
+  )
+}
+
+const AssetAllocation = (
+  props,
+  initialInterval = 0,
+  initialUser = 0,
+  initialPortfolio = []
+) => {
+  const [intervalId, setIntervalId] = useState(initialInterval)
+  const [currentUser, setCurrentUser] = useState(initialUser)
+  const [portfolio, setPortfolio] = useState(initialPortfolio)
+  const intervalFunc = async () => {
+    const callBack = (func, userId) => {
+      func(userId)
+    }
+    props.getPortfolio(currentUser)
+    const currentInterval = setInterval(() => {
+      callBack(props.getPortfolio, props.userId)
+    }, 50000)
+    await setIntervalId(currentInterval)
+  }
+  useEffect(async () => {
+    setPortfolio(props.portfolio)
+    setCurrentUser(props.userId)
+    await intervalFunc()
+    return () => {
+      clearInterval(intervalId)
+    }
+  }, [])
+  let myData
+  if (props.portfolio.length) {
+    myData = props.portfolio.reduce((accum, val) => {
+      accum.push({angle: val[1]})
+      return accum
+    }, [])
+  } else {
+    myData = [{angle: 0}, {angle: 0}, {angle: 100}]
+  }
+  return (
+    <div>
+      <AssetAllocationModal />
+      <RadialChart
+        animation
+        colorType="category"
+        colorDomain={[0, 1, 2, 3, 4, 5, 6]}
+        colorRange={myPalette}
+        className="templateAssetAllocation"
+        data={myData}
+        width={300}
+        height={300}
+      />
+    </div>
+  )
+}
+
+/*
 class AssetAllocation extends Component {
   constructor(props) {
     super(props)
@@ -59,9 +154,6 @@ class AssetAllocation extends Component {
     await this.intervalFunc()
   }
 
-  show = size => () => this.setState({size, open: true})
-
-  close = () => this.setState({open: false})
 
   render() {
     let myData
@@ -74,49 +166,9 @@ class AssetAllocation extends Component {
       myData = [{angle: 0}, {angle: 0}, {angle: 100}]
     }
 
-    const {open, size} = this.state
 
     return (
-      <div>
-        <h4>
-          Portfolio Allocation{' '}
-          <Button
-            onClick={this.show('tiny')}
-            id="help-btn"
-            icon="question circle outline icon"
-          />{' '}
-        </h4>
-        <div>
-          <Transition.Group
-            open={open}
-            transition="horizontal-flip"
-            duration={1000}
-          >
-            {open && (
-              <Modal size={size} open={open} onClose={this.close}>
-                <Modal.Header>
-                  This section contains a graphical division of your portfolio
-                </Modal.Header>
-                <Modal.Content>
-                  <p>
-                    This pie chart graph will automatically calculate and
-                    display how your initial cash was divided between your
-                    bought stocks.
-                  </p>
-                </Modal.Content>
-                <Modal.Actions>
-                  <Button
-                    onClick={this.close}
-                    positive
-                    icon="checkmark"
-                    labelPosition="right"
-                    content="Got it!"
-                  />
-                </Modal.Actions>
-              </Modal>
-            )}
-          </Transition.Group>
-        </div>
+        <AssetAllocationModal />
         <RadialChart
           animation
           colorType="category"
@@ -131,6 +183,7 @@ class AssetAllocation extends Component {
     )
   }
 }
+*/
 
 const mapStateToProps = state => {
   return {
